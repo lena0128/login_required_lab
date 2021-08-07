edfa4047e7957a53fbd55185cafbbca0a92f18f2
Lena Hou <lenahou2013@gmail.com>
2021-08-07 19:10:57 -0400

    commit tree e38e9b9b4278d408e57ccbbda6f322ffd5abdfb8

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,11 @@
+class SecretsController < ApplicationController
+   # require_login is an ApplicationController method
+   before_action :require_login
+
+  def new
+  end
+
+  def show
+  end
+
+end
